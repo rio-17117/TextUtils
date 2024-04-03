3d18cdd268770af74470883c2cb2c0e6acf3d729
rio-17117 <mandeepsvirdi17@gmail.com>
2024-04-03 18:04:11 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { Toaster } from 'react-hot-toast';
 import './App.css';
 import Navbar from "./components/Navbar";
 import TextArea from "./components/TextArea";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -12,11 +18,14 @@ function App() {
   };
 
   return (
-    <div className={`app-container ${darkMode ? 'dark-mode' : ''}`}>
-      <Navbar mode={darkMode ? 'dark' : 'light'} toggleMode={toggleMode} />
-      <TextArea mode={darkMode ? 'dark' : 'light'} toggleMode={toggleMode} />
-      <Toaster />
-    </div>
+    <Router>
+      <div className={`app-container ${darkMode ? 'dark-mode' : ''}`}>
+        <Routes>
+          <Route path="/about" element={<><Navbar mode={darkMode ? 'dark' : 'light'} toggleMode={toggleMode} /><Toaster /></>} />
+          <Route path="/" element={<><Navbar mode={darkMode ? 'dark' : 'light'} toggleMode={toggleMode} /><TextArea mode={darkMode ? 'dark' : 'light'} toggleMode={toggleMode} /></>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
